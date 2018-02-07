7cf1e4ceef507cfab6679d620f247acf5497cf2b
Liuqing650 <1006822621@qq.com>
2018-02-07 21:59:53 +0800

    test

--- src/assets/chart.js
+++ src/assets/chart.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import G6 from '@antv/g6';
 import SideTool from './tool';
 import styles from './index.less';
+import Tooltip from 'components/lib/Tooltip';
+import LinkModule from 'components/common/jumps/nameAndIconLinkModule';
 const Global = G6.Global;
 function generateUniqueId(isDeep) {
   return isDeep ? `rc-g6-1` : `rc-g6-0`;
 }
+// 设置全局节点样式
 Global.nodeStyle = {
   stroke: '#222',
   fill: '#fff',
@@ -30,16 +33,17 @@ G6.registerNode('treeNode', {
       line: model.treeInfo && model.treeInfo.line ? model.treeInfo.line : 0,
       text: model.treeInfo && model.treeInfo.text ? model.treeInfo.text : '',
     };
-    const nameGroup = group.addGroup();
-    const lineHeight = 20;
-    const marginRight = 10;
-    const padding = 6;
+    const detailGroup = group.addGroup();
     const length = `${model.treename}`.length;
+    const anchorPoints = [];
+    // 位置
+    const lineHeight = 20; // 间隔高度
+    const margin = 10;
+    const padding = 6;
     let fontHeight;
-    let anchorPoints = [];
     let title;
     let titleBox;
-    let nameBox;
+    let detailBox;
     let width;
     let height;
     title = group.addShape('text', {
@@ -53,25 +57,26 @@ G6.registerNode('treeNode', {
         textAlign: 'center'
       }
     });
-    nameGroup.addShape('text', {
+    detailGroup.addShape('text', {
       attrs: {
-        x: cfgx,
-        y: cfgy + 10,
+        x: cfgx + margin,
+        y: cfgy + margin,
         text: labelConfig.text,
         fill: '#212121',
-        textBaseline: 'top'
+        textBaseline: 'top',
+        textAlign: 'left'
       }
     });
 
     titleBox = title.getBBox();
-    nameBox = nameGroup.getBBox();
-    const maxwidth = titleBox.width > nameBox.width ? titleBox.width : nameBox.width;
-    width = maxwidth + 3 * marginRight + 2 * padding;
-    height = Math.max(nameBox.height) + 2 * padding + titleBox.height;
-    fontHeight = nameGroup.get('children')[0].getBBox().height;
+    detailBox = detailGroup.getBBox();
+    const maxwidth = titleBox.width > detailBox.width ? titleBox.width : detailBox.width;
+    width = maxwidth + 3 * margin + 2 * padding;
+    height = Math.max(detailBox.height) + 2 * padding + titleBox.height;
+    fontHeight = detailGroup.get('children')[0].getBBox().height;
 
     title.translate(0, -height / 2 + padding);
-    nameGroup.translate(-width / 2 + padding, -height / 2 + titleBox.height + padding);
+    detailGroup.translate(-width / 2 + padding, -height / 2 + titleBox.height + padding);
     backRect.attr({
       x: cfgx - width / 2,
       y: cfgy - height / 2,
@@ -80,7 +85,7 @@ G6.registerNode('treeNode', {
       fill: cfg.color
     });
     const points = {
-      moreLine: (titleBox.height + labelConfig.line * (nameBox.height + lineHeight - fontHeight) / length + fontHeight / 2 + 3 * padding) / height,
+      moreLine: (titleBox.height + labelConfig.line * (detailBox.height + lineHeight - fontHeight) / length + fontHeight / 2 + 3 * padding) / height,
       oneLine: (titleBox.height + fontHeight / 2 + 3 * padding) / height,
     };
     anchorPoints.push([0, labelConfig.exist ? points.moreLine : points.oneLine]);
@@ -88,87 +93,19 @@ G6.registerNode('treeNode', {
     return backRect;
   }
 });
-G6.registerEdge('treeEdge', {
-  draw(cfg, group) {
-    // return group.addShape('polyline', {
-    //   attrs: {
-    //     points: [
-    //       [cfg.points[0].x, cfg.points[0].y],
-    //       [cfg.points[1].x, cfg.points[1].y]
-    //     ],
-    //     zIndex: -1,
-    //     stroke: '#222',
-    //     lineWidth: 1
-    //   }
-    // });
-    // 三阶贝赛尔曲线
-    const node = {
-      x: (cfg.points[0].x + cfg.points[1].x) / 4,
-      y: (cfg.points[0].y + cfg.points[1].y) / 4
-    };
-    console.log(cfg.points);
-    // 获取首个4/1点
-    const cubic = {
-      p1: [cfg.points[0].x, cfg.points[0].y],
-      p2: [node.x, node.y],
-      p3: [node.x, node.y],
-      p4: [cfg.points[1].x, cfg.points[1].y],
-    };
-    return group.addShape('cubic', {
-      attrs: {
-        p1: cubic.p1,
-        p2: cubic.p2,
-        p3: cubic.p3,
-        p4: cubic.p4,
-        zIndex: -1,
-        stroke: '#222',
-        lineWidth: 1
-      }
-    });
-  },
-  afterDraw(cfg, group) {
-    if (cfg.label) {
-      const isFr = cfg.label.isFr;
-      const node = {
-        x: (cfg.points[0].x + cfg.points[1].x) / 2 - 30,
-        y: (cfg.points[0].y + cfg.points[1].y) / 2
-      };
-      if (!isFr) {
-        node.y -= 20;
-      }
-      group.addShape('rect', {
-        attrs: {
-          x: node.x - 10,
-          y: node.y - 5,
-          width: 100,
-          height: cfg.label.isFr ? 26 : 50,
-          stroke: 'black',
-          fill: '#FFF'
-        }
-      });
-      group.addShape('text', {
-        attrs: {
-          x: node.x,
-          y: node.y,
-          fill: 'green',
-          lineWidth: 100,
-          text: cfg.label.text,
-          textAlign: 'left',
-          textBaseline: 'top'
-        }
-      });
-    }
-  },
-});
 class Chart extends Component {
+  static propTypes = {
+    dataLength: PropTypes.number,
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       node: {},
-      isSave: false, // 存储状态
-      isRead: false, // 阅读模式
+      downloadLoading: false,
       url: '',
       ratio: 1,
+      zoomScale: 1, // 初始缩放比例,
     };
     // id生成函数
     this.graphId = generateUniqueId();
@@ -177,7 +114,8 @@ class Chart extends Component {
     this.deepGraphId = generateUniqueId(true);
     this.deepGraph = null; // 用作下载
     this.deepGraphContainer = null;
-
+    this.center = { x: 480, y: 300 };
+    this.position = { x: 0, y: 0 };
     this.nodeStyle = {
       lineWidth: 1
     };
@@ -193,24 +131,108 @@ class Chart extends Component {
       this.initTree(this.props);
     }
   }
-  // 跳转页面
+  /**
+   * G6 事件函数
+   * 1. 鼠标左击击事件 onLeftClick
+   * 2. 鼠标右击事件 onNodeClick
+   * 3. 光标移入事件 onMouseEnter
+   * 4. 光标移出事件 onMouseLever
+   * 5. 光标滚动事件 onMouseWheel
+   * 6. 图片下载按钮事件 download
+   */
   onNodeClick = (node) => {
-    const { item, itemType } = node;
+    Tooltip.remove();
+    const { item, itemType, domEvent } = node;
+    console.log('node---->', node);
+    console.log('item---->', item);
     if (item && item._attrs && itemType === 'node') {
-      console.log(node);
+      const { model } = item._attrs;
+      domEvent.preventDefault();
+      if (model.dataType === 1) {
+        Tooltip.show({
+          pageX: domEvent.pageX,
+          pageY: domEvent.pageY,
+          component: (
+            <LinkModule
+              exclude={model.root ? ['report'] : []}
+              companyName={model.treename}
+              linkType="open"
+              mode="vertical"
+              cbFunc={Tooltip.remove} />
+          ),
+        });
+      }
     }
   }
-
-  // 初始化Tree
+  onMouseEnter = (even) => {
+    if (even.itemType !== 'node') {
+      return;
+    }
+    const { model } = even.item._attrs;
+    const element = this.graph.get('el');
+    if (model.dataType === 1) {
+      element.style.cursor = 'pointer';
+    }
+    // this.graph.refresh();
+  }
+  onMouseLever = (even) => {
+    if (even.itemType !== 'node') {
+      return;
+    }
+    const element = this.graph.get('el');
+    element.style.cursor = 'move';
+  }
+  onMouseWheel = (event, isFirefox) => {
+    const { zoomScale } = this.state.zoomScale;
+    const scale = this.graph.getScale();
+    if (!isFirefox && zoomScale !== scale) {
+      const handleScale = Math.round(parseFloat(scale + 0.2) * 100) / 100;
+      this.setState({
+        ratio: handleScale
+      });
+    } else { // 处理火狐浏览器
+      const delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
+      const zoom = delta > 0 ? 'add' : 'sub';
+      this.focus(scale, zoom);
+    }
+  }
+  download = () => {
+    this.setState({
+      downloadLoading: true
+    });
+    if (this.deepGraph) {
+      this.deepGraph.destroy();
+    }
+    // 分析放大倍数
+    const { dataLength } = this.props;
+    console.log('dataLength------>', dataLength);
+    const zoomScale = dataLength >= 100 ? 0.223 : 0.6 - (2 / (10 - dataLength / 10));
+    const zoom = dataLength > 10 ? Math.round(parseFloat(dataLength * zoomScale) * 100) / 100 : 3;
+    this.createDeepTree(this.props, zoom);
+    this.graph.autoZoom(); // 可视图层缩放到适应屏幕
+    const saveName = this.findRootInfo('treename');
+    setTimeout(() => {
+      this.downloadImage(saveName ? saveName : '');
+    }, zoom > 3 ? zoom * 500 : 300);
+  };
+  /**
+   * 初始化图形
+   * initTree 初始化可视图形（显示）
+   * @param {*Obejct} props 数据
+   *
+   * createDeepTree 创建一个下载图形（隐藏）
+   * @param {*Obejct} props 数据
+   * @param {*number} zoom  放大系数
+   */
   initTree(props) {
     const graph = new G6.Tree({
       id: this.graphId,
       fitView: 'autoZoom',
       ...props
     });
-    graph.tooltip(true);
+    graph.tooltip(false);
     this.graphContainer = graph.get('graphContainer');
-    graph.addBehaviour('default', ['clickBlankClearActive']);
+    graph.addBehaviour('default', ['dragCanvas']);
     graph.node().shape('treeNode');
     graph.node().label((data) => {
       return {
@@ -218,23 +240,10 @@ class Chart extends Component {
         nodeInfo: data,
       };
     }).style(this.nodeStyle);
-    graph.edge().shape('treeEdge');
-    graph.edge().label((data) => {
-      const allData = [this.graph.save().source];
-      const node = this.findNodeDataById(allData, data.target);
-      const text = node.treeInfo ? node.treeInfo.text : '';
-      if (text) {
-        return {
-          text: text,
-          layer: node.layer, // 自定义属性 位于第几层（unuse）
-          isFr: node.treeInfo.isFr, // 自定义属性 判断是否是法人
-          fill: 'green',
-          textAlign: 'left'
-        };
-      }
-    }).style({
+    graph.edge().shape('smoothArrow').style({
+      arrow: true,
       lineWidth: 1,
-      stroke: '#222'
+      full: '#979797'
     });
     graph.edge().tooltip((data) => {
       const allData = [this.graph.save().source];
@@ -249,19 +258,28 @@ class Chart extends Component {
     graph.source(props.data);
     this.graph = graph;
     this.graph.render();
-    this.graph.on('click', this.onNodeClick);
+    this.setState({
+      zoomScale: this.graph.getScale()
+    });
+    this.graph.on('contextmenu', this.onNodeClick);
+    // this.graph.on('click', this.onLeftClick);
+    this.graph.on('itemmouseenter', this.onMouseEnter);
+    this.graph.on('itemmouseleave', this.onMouseLever);
+    this.graph.on('mousewheel', this.onMouseWheel);
+    if (/Firefox/i.test(navigator.userAgent)) {
+      this.graphContainer.addEventListener('DOMMouseScroll', (event) => {
+        this.onMouseWheel(event, true);
+      });
+    }
   }
-  createDeepTree = (props) => {
+  createDeepTree = (props, zoom) => {
     const deepGraph = new G6.Tree({
       id: this.deepGraphId,
-      fitView: {
-        x: 0,
-        y: 0
-      },
+      fitView: 'autoZoom',
       ...props,
-      height: 1000
+      width: 1000 * zoom,
+      height: props.height
     });
-    this.deepGraphContainer = deepGraph.get('graphContainer');
     deepGraph.node().shape('treeNode');
     deepGraph.node().label((data) => {
       return {
@@ -269,91 +287,94 @@ class Chart extends Component {
         nodeInfo: data,
       };
     }).style(this.nodeStyle);
-    // deepGraph.edge().shape('smooth');
-    deepGraph.edge().shape('treeEdge');
-
-    deepGraph.edge().label((data) => {
-      const allData = [this.deepGraph.save().source];
-      const node = this.findNodeDataById(allData, data.target);
-      const text = node.treeInfo ? node.treeInfo.text : '';
-      if (text) {
-        return {
-          text: text,
-          layer: node.layer, // 自定义属性 位于第几层（unuse）
-          isFr: node.treeInfo.isFr, // 自定义属性 判断是否是法人
-          fill: 'green',
-          textAlign: 'left'
-          // rotate: this.angle(parent, node)
-        };
-      }
-    }).style({
+    deepGraph.edge().shape('smooth').style({
+      arrow: true,
       lineWidth: 1,
-      stroke: '#222'
-    });
-    deepGraph.edge().color((data) => {
-      const allData = [this.deepGraph.save().source];
-      const node = this.findNodeDataById(allData, data.target);
-      return [['路径标识', node.treename]];
+      full: '#979797'
     });
     deepGraph.source(props.data);
     this.deepGraph = deepGraph;
     this.deepGraph.render();
   }
-  // 图片下载按钮事件
-  download = () => {
-    this.createDeepTree(this.props);
-    this.graph.autoZoom(); // 缩放到适应大小
-    this.deepGraph.autoSize();
-    setTimeout(() => {
-      this.downloadImage();
-    }, 1000);
-  };
-  // 下载图片
-  downloadImage = () => {
-    const canvasArr = this.deepGraph.get('graphContainer').getElementsByTagName('canvas');
-    canvasArr[0].getContext('2d').drawImage(canvasArr[1], 0, 0);
-    const dataURL = canvasArr[0].toDataURL('image/jpeg');
-    const link = document.createElement('a');
-    const saveName = 'graph.jpeg';
-    link.download = saveName;
-    link.href = dataURL.replace('image/jpeg', 'image/octet-stream');
-    link.click();
-  }
   /**
-   * 缩放图形
+   * 缩放图形、下载图片
    * ratio 缩放比例
    * zoom 放大|缩小
+   * downloadImage 图形转换下载
    */
   focus = (ratio, zoom) => {
     let scale = ratio;
-    if (scale < 0) {
-      scale = 0;
+    if (isNaN(ratio)) {
+      return false;
     }
-    if (scale > 10) {
-      scale = 10;
-    }
+    const check = (num) => {
+      if (num <= 0) {
+        return 0;
+      }
+      if (num >= 10) {
+        return 10;
+      }
+      return num;
+    };
+    scale = check(scale);
     if (zoom === 'add' && scale >= 0 && scale < 10) {
-      scale = Math.round(parseFloat(scale + 0.5) * 100) / 100;
+      scale = Math.round(parseFloat(scale + 0.2) * 100) / 100;
     }
-    if (zoom === 'sub' && scale <= 10 && scale > 0) {
-      scale = Math.round(parseFloat(scale - 0.5) * 100) / 100;
+    if (zoom === 'sub' && scale <= 10 && scale >= 0) {
+      scale = Math.round(parseFloat(scale - 0.2) * 100) / 100;
     }
-    this.zoom(scale);
+    scale = check(scale);
+    this.zoom(scale, this.graph);
     this.setState({
       ratio: scale
     });
   }
-  zoom = (ratio) => {
+  /**
+   * 1. 初始化的时候获取到所有节点的model位置。
+   * 2. 反转
+   */
+  zoom = (ratio, graph) => {
+    const zoom = this.center;
     const matrix = new G6.Matrix.Matrix3();
+    matrix.translate(-zoom.x, -zoom.y);
     matrix.scale(ratio, ratio);
-    matrix.translate(480, 300);
-    this.graph.updateMatrix(matrix);
-    this.graph.refresh();
+    matrix.translate(zoom.x, zoom.y);
+
+    graph.updateMatrix(matrix);
+    graph.refresh();
+  }
+  downloadImage = (filename) => {
+    const canvasArr = this.deepGraph.get('graphContainer').getElementsByTagName('canvas');
+    const { height, width } = canvasArr[0];
+    // 设置图形层背景颜色，合并为一个canvas
+    canvasArr[0].style.backgroundColor = '#fff';
+    canvasArr[1].style.backgroundColor = 'transparent';
+    canvasArr[0].getContext('2d').drawImage(canvasArr[1], 0, 0);
+    // 创建新画布，矩形填充整个画布，再次合并所有画布
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, width, height);
+    ctx.drawImage(canvasArr[0], 0, 0);
+    console.log(filename);
+    // 创建并下载图片
+    const dataURL = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    const saveName = `${filename}-股权结构图.jpeg`;
+    link.download = saveName;
+    link.href = dataURL.replace('image/jpeg', 'image/octet-stream');
+    link.click();
+    this.setState({
+      downloadLoading: false
+    });
   }
   /**
    * 工具区
    * 1. 根据id查找节点 findNodeDataById
-   * 2. 获取角度 angle
+   * 2. 查找根信息 findRootInfo
+   * 3. 获取角度 angle
    */
 
   findNodeDataById = (nodeData, nodeId) => {
@@ -375,6 +396,14 @@ class Chart extends Component {
     loop(nodeData);
     return output;
   };
+  findRootInfo = (attr) => {
+    const allData = this.graph.save().source;
+    if (allData.root) {
+      return attr ? allData[attr] : allData;
+    }
+    console.log('没有数据');
+    return null;
+  }
   angle = (start, end) => {
     const diffX = end.x - start.x;
     const diffY = end.y - start.y;
@@ -382,9 +411,10 @@ class Chart extends Component {
   }
 
   render() {
+    const { ratio, downloadLoading } = this.state;
     const self = this;
-    const { ratio } = this.state;
     const sideToolProps = {
+      loading: downloadLoading,
       scale: Math.round(ratio * 10),
       onClick(even) {
         switch (even) {
@@ -403,9 +433,9 @@ class Chart extends Component {
       }
     };
     return (
-      <div>
+      <div className={styles.chartWrap}>
         <SideTool {...sideToolProps} />
-        <div id={this.graphId} className={styles.chartWrap}></div>
+        <div id={this.graphId} className={styles.chart}></div>
         <div id={this.deepGraphId} style={{ display: 'none' }}></div>
       </div>
     );
--- src/assets/tool.js
+++ src/assets/tool.js
@@ -26,84 +26,159 @@ export default SideTool;
 
 
 
+import React from 'react';
+import TreeChart from './tree';
 
-
-
-
-
-
-
-
-
-
-
-
-
-@color: #42a5f5;
-.unselect() {
-  -moz-user-select: none;
-  -o-user-select:none;
-  -khtml-user-select:none;
-  -webkit-user-select:none;
-  -ms-user-select:none;
-  user-select:none;
-}
-.boxWrap {
-  width: 50px;
-  text-align: center;
-  border: 1px solid #eeeeee;
-}
-.chartWrap {
-  border: none;
-  cursor: pointer;
-}
-.toolWrap {
-  position: absolute;
-  z-index: 1000;
-  top: 200px;
-  right: 50px;
-  .box {
-    .unselect();
-    .toolBtn {
-      .boxWrap;
-      height: 50px;
-      margin-top: 10px;
-      cursor: pointer;
-      &:hover {
-        border-color: @color;
+const Chart = ({
+  chartData,
+  dataLength
+}) => {
+  const treeChartProps = {
+    data: chartData,
+    dataLength: dataLength,
+    height: 600,
+    fitViewPadding: 30,
+    layoutCfg: {
+      direction: 'V',
+      getHGap: () => {
+        return 50;
+      },
+      getVGap: () => {
+        return 80;
       }
-      transition: border-color 0.3s;
-      i {
-        display: inline-block;
-        width: 32px;
-        height: 32px;
-        line-height: 32px;
-        vertical-align: text-top;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        color: #42a5f5;
+    },
+    grid: null
+  };
+  return (
+    <div>
+      {dataLength > 0 ? <TreeChart {...treeChartProps} /> : null}
+    </div>
+  );
+};
+export default Chart;
+
+
+
+
+
+
+
+
+
+
+
+  // 数据总长度，用于下载图形时计算放大比例
+  @observable dataLength = 0;
+  @observable iserror = false;
+  @action.bound structureMapping(data) {
+    this.baseData = {};
+    const mapInfo = {
+      companyName: data.companyName ? data.companyName : '--', // 企业名称
+      frname: data.frName ? `${data.frName}(法人代表)` : '--', // 华人代表
+      entinvList: data.entinvItemList && data.entinvItemList.length > 0 ? data.entinvItemList : false, // 企业对外投资
+      shareList: data.shareHolderList && data.shareHolderList.length > 0 ? data.shareHolderList : false, // 股东
+      frinvList: data.frinvList && data.frinvList.length > 0 ? data.frinvList : false, // 法人对外投资
+      frPositionList: data.frPositionList && data.frPositionList.length > 0 ? data.frPositionList : false, // 法人在外任职
+    };
+    const frInfo = {
+      treename: mapInfo.frname,
+      color: '#E5E5E5',
+      dataType: 2, // 数据类型： 公司为1， 人名为2
+      children: []
+    };
+    const config = {
+      'date': '出资日期',
+      'invest': '投资',
+      'ratio': '占比',
+    };
+    // 获取label数据
+    const handleLabel = (itemData, isFr = false) => {
+      const output = {};
+      const temp = {};
+      output.isFr = isFr;
+      output.line = isFr ? 1 : 2;
+      if (isFr) {
+        output.text = '担任法人';
+        return output;
       }
-      .downloadIcon {
-        background-image: url(imgs/bannerInfo/download.png);
+      const modifyNumber = (value, unit = '万元') => {
+        return isNaN(Number(value)) || Number(value).toFixed(2) === '0.00' ? '--' : Number(value).toFixed(2) + unit;
+      };
+      temp.date = itemData.conDate ? itemData.conDate : '--';
+      temp.invest = itemData.subConam ? modifyNumber(itemData.subConam) : '--';
+      temp.ratio = itemData.fundedRatio ? itemData.fundedRatio : '--';
+      output.text = `${config.invest}${temp.invest}(${config.ratio}: ${temp.ratio})\n${config.date}: ${temp.date}`;
+      return output;
+    };
+    /**
+     * 检验公司名称
+     * @param {*String} query 待测字符串
+     * @param {*Number} minLen 最小长度
+     * @return true | false
+     */
+    const checkCompanyName = (query, minLen = 6) => {
+      if (!query) {
+        return false;
       }
+      const length = `${query}`.length;
+      const cond0 = Boolean(length < minLen); // 字符串长度 < minLen
+      const cond1 = !Boolean(/[^a-zA-Z]/.test(query)); // 全英文
+      const cond2 = Boolean(`${query.replace(/[^0-9]+/g, '')}`.length >= length * 0.6); // 数字占比 >= 0.6;
+      const cond3 = Boolean(/[~!@#$%\^&\*\-_=+<>,;:、。?/\\|～＠＃￥％＾＆＊＿＝＋＜＞，．。；：？／＼｜]/.test(query)); // 包含特殊字符
+      const cond4 = Boolean(query.match(/[\(（]?\d{1,2}[\.\)）\u2E80-\u9FFF\s]/)); // 数字在前面的错误情况
+      const cond5 = Boolean(query.match(/[\u2E80-\u9FFF]+[\da-zA-Z]?[\d]$/)); // 数字在最后的错误情况
+      return !(cond0 || cond1 || cond2 || cond3 || cond4 || cond5);
+    };
+
+    // 获取股东数据
+    const getShareData = (arrData, keys, dataType = 1) => {
+      arrData.map((item) => {
+        let type = dataType;
+        if (dataType === 2 && checkCompanyName(item[keys])) {
+          --type;
+        }
+        item.treename = item[keys];
+        item.treeInfo = handleLabel(item);
+        item.treeKey = keys;
+        item.color = '#FFFFFF';
+        item.dataType = type;
+        this.baseData.children.push(item);
+      });
+    };
+    // 获取法人信息
+    const modifyFrData = (arrData, keys, isFr, dataType = 1) => {
+      arrData.map((item) => {
+        item.treename = item[keys];
+        item.treeInfo = handleLabel(item, isFr);
+        item.treeKey = keys;
+        item.color = '#FFFFFF';
+        item.dataType = dataType;
+        frInfo.children.push(item);
+      });
+    };
+    // 获取法人信息
+    if (mapInfo.frinvList) { // 法人对外投资
+      modifyFrData(mapInfo.frinvList, 'entName', 1);
     }
-    .zoomContent {
-      .boxWrap;
-      padding: 10px 0;
-      color: #42a5f5;
-      i {
-        text-align: center;
-        font-size: 20px;
-        cursor: pointer;
-      }
-      p {
-        text-align: center;
-        font-size: 12px;
-      }
-      .zoomText {
-        margin: 10px 0;
-      }
+    if (mapInfo.frPositionList) { // 法人在外任职
+      modifyFrData(mapInfo.frPositionList, 'entName', 1);
     }
+
+    // 存入数据
+    this.baseData.id = 'root';
+    this.baseData.treename = mapInfo.companyName;
+    this.baseData.dataType = 1;
+    this.baseData.color = '#E5E5E5';
+    this.baseData.children = [];
+    this.baseData.children.push(frInfo);
+    // 股东数据
+    if (mapInfo.shareList) { // 股东
+      getShareData(mapInfo.shareList, 'shareholderName', 2);
+    }
+    if (mapInfo.entinvList) { // 企业对外投资
+      getShareData(mapInfo.entinvList, 'entName', 1);
+    }
+    this.dataLength = Math.round(this.baseData.children.length / 2) + frInfo.children.length;
+    console.log('baseData---->', this.baseData);
+    // console.log('dataLength---->', this.dataLength);
   }
-}
